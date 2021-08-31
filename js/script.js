08a4c0f72b90a51f91b2982e96ca7a77004eefc1
mamun8893 <0316361936@student.wub.edu.bd>
2021-09-01 01:30:59 +0600

    show result

--- js/script.js
+++ js/script.js
@@ -1,26 +1,49 @@
+const message = document.getElementById("message");
+
 document.getElementById("search-btn").addEventListener("click", function() {
+    const loading = document.getElementById("loading");
     const inputField = document.getElementById("input-field");
-    const inputFieldValue = inputField.value;
+    const countryName = inputField.value;
     inputField.value = "";
-    const url = `https://api.covid19api.com/total/country/${inputFieldValue}`;
-    fetch(url)
-        .then((res) => res.json())
-        .then((data) => displayData(data));
+    const url = `https://api.covid19api.com/total/country/${countryName}`;
+
+    if (countryName == "") {
+        message.innerText = "Please Enter Your Country Name";
+        message.style.color = "red";
+    } else {
+        loading.style.display = "block";
+        fetch(url)
+            .then((res) => res.json())
+            .then((data) => displayData(data, countryName));
+    }
 });
 
-const displayData = (data) => {
-    console.log(data);
+const displayData = (data, countryName) => {
     const latestData = data[data.length - 1];
+    console.log(latestData);
+    const loading = document.getElementById("loading");
     const searchResult = document.getElementById("covid-result");
-    searchResult.innerHTML = `
+    if (Array.isArray(data) == false) {
+        message.innerText = `Sorry ${countryName}  is not found.`;
+        message.style.color = "red";
+        loading.style.display = "none";
+    } else if (Array.isArray(data)) {
+        message.innerText = `Result: ${latestData.Country}`;
+        message.style.color = "green";
+        searchResult.innerHTML = `
+    <div class="item">
+         <h4>Country: ${latestData.Country}</h4>
+    </div>
      <div class="item">
-         <h4>Total Cases:${latestData}</h4>
+         <h4>Total Cases: ${latestData.Confirmed}</h4>
     </div>
     <div class="item">
-        <h4>Total Recovery:</h4>
+        <h4>Total Recovery: ${latestData.Active}</h4>
     </div>
     <div class="item">
-         <h4>Total Deaths:</h4>
+         <h4>Total Deaths: ${latestData.Deaths}</h4>
     </div>
     `;
+        loading.style.display = "none";
+    }
 };
